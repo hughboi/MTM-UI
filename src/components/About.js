d3713b903430cbcd192133c5bd64f8c353910b22
hughboi <hdc@ualberta.ca>
2018-01-24 13:29:08 -0700

    my amazing website

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,19 @@ export default class About extends React.Component {
   
   render() {
     return (
-      <div></div>
+      <div>
+        <div className = 'container'>
+          <div className = 'row'>
+            <div className = 'col-lg-12 text-center'>
+              <h2> About </h2>
+            </div>
+
+            <div className = 'col-lg-12 text-center'>
+              <h2> Wow, look at my beautiful website. So beautiful. </h2>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
   
